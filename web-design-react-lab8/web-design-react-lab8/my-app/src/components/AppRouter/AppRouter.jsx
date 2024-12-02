1a10befaf92033fc287025d176ebb172976f35d9
mlshvik <viktoriia.malish.ir.2023@lpnu.ua>
2024-12-02 22:27:16 +0200

    added lab 7-8

---
+++ web-design-react-lab8/web-design-react-lab8/my-app/src/components/AppRouter/AppRouter.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import HomePage from "../../pages/HomePage";
+import CatalogPage from "../../pages/CatalogPage";
+import ItemPage from "../../pages/ItemPage";
+
+
+const AppRouter = () => {
+    const routes = [
+        {path: "/", element: <HomePage/>},
+        {path: "/catalog", element: <CatalogPage/>},
+        {path: "/catalog/:id", element:<ItemPage/>}
+    ]
+    return (
+        <Routes>
+            {routes.map(route =>
+                <Route element={route.element} path={route.path}/>)}
+
+            <Route element={<HomePage/>} path="*"/>
+        </Routes>
+
+    );
+};
+
+export default AppRouter;
